80f7596eae136a2532cf197dfa4c660d36ae1550
grupodog <grupodog@gmail.com>
2021-02-20 11:57:22 -0600

    cahnges links hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -10,9 +10,9 @@ function HeroSection(props) {
     <div className="md:grid md:grid-rows-2  md:ml-6 md:p-8 md:justify-center md:items-center">
     <h2 className="grid text-sm mt-2 md:mx-8 px-8 py-6">Convencidos de que la Arquitectura transmite sentimientos y emociones creamos <h2 className="font-extrabold">Arqca.</h2> un taller de Arquitectura Integral, que tiene como objetivo diseñar espacios Funcionales, Estéticos e integrales con el medio Ambiente.</h2>
     <div className="inline-grid grid-cols-3 pl-8 justify-between">
-    <a href="/contacto" className="p-2 text-blue-600"> Contacto</a>
-    <a href="/archive" className="p-2 text-blue-600"> Proyectos</a>
-    <a href="/conocenos" className="p-2 text-blue-600"> Nosotros</a>
+    <a href="/contacto" className="p-2 font-extrabold underline"> Contacto</a>
+    <a href="/archive" className="p-2 font-extrabold underline"> Proyectos</a>
+    <a href="/conocenos" className="p-2 font-extrabold underline"> Nosotros</a>
     </div>
     </div>
     </div>
